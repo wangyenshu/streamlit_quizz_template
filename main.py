eb39a733a7db6b3f210f1b0cbaf15ff653264cb6
wangyanshu <155622798+wangyenshu@users.noreply.github.com>
2024-07-26 14:54:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,7 @@ import streamlit as st
 import json
 import random
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 
 def run():
     st.set_page_config(
@@ -30,7 +19,11 @@ st.markdown("""
 div.stButton > button:first-child {
     display: block;
     margin: 0 auto;
-
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
 </style>
 """, unsafe_allow_html=True)
 
