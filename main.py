2328fe4c000f1989d872d76925fda71a217c6cdf
wangyanshu <155622798+wangyenshu@users.noreply.github.com>
2024-07-26 14:51:09 +0800

    hide github icon

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import streamlit as st
 import json
 import random
 
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 def run():
     st.set_page_config(
         page_title="Streamlit quizz app",
